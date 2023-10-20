fe061e17c4ca8aa153964e8dcebdb8c1e089061b
Jaya Kandpal <jaya.kandpal99@gmail.com>
2023-10-20 14:06:25 +0530

    Made changes in quiz_brain.py so that the game continues even if the answer is incorrect. Added a new method - still_continue.

--- quiz_brain.py
+++ quiz_brain.py
@@ -2,24 +2,24 @@ class QuizBrain:
     def __init__(self, question_list):
         self.question_number = 0
         self.question_list = question_list
+        self.score = 0
+
+    def still_continue(self):
+        if self.question_number < len(self.question_list):
+            QuizBrain.next_question(self)
+        else:
+            print(f"You completed the quiz! Your final score is {self.score}/{len(self.question_list)}.")
 
     def next_question(self):
         current_question = self.question_list[self.question_number]
-        question = input(f"Q.{self.question_number + 1} {current_question.text} (True/False)?: ")
+        self.question_number += 1
+        question = input(f"Q.{self.question_number} {current_question.text} (True/False)?: ")
+
         if question == current_question.answer:
-            print(f"You got it right!\nThe correct answer was: {current_question.answer}")
+            self.score += 1
+            print(f"You got it right!\nThe correct answer was: {current_question.answer}. Your score is {self.score}")
+        elif question != current_question.answer:
+            print(f"That was incorrect.\nThe correct answer was: {current_question.answer}. "
+                  f"Your score is {self.score}.")
 
-            if self.question_number < 11:
-                print(f"Your score is {self.question_number + 1}")
-                self.question_number += 1
-                QuizBrain.next_question(self)
-            else:
-                print(f"You got all the answers right! Your final score is {self.question_number}")
-        else:
-            print(f"That was incorrect. Your final score is {self.question_number}.")
-
-
-
-
-
-
+        QuizBrain.still_continue(self)
