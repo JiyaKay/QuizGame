c5b1e22b60c0b513a0925c4c20bcc9b9f5c45a49
Jaya Kandpal <jaya.kandpal99@gmail.com>
2023-10-23 19:24:43 +0530

    Added new questions to data.py from open trivia database. Changed the 'key' name in main.py with the key name for question and answer in the new set of questions.

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from quiz_brain import QuizBrain
 
 question_bank = []
 for q in question_data:
-    question_text = q["text"]
-    question_ans = q["answer"]
+    question_text = q["question"]
+    question_ans = q["correct_answer"]
     question = Question(question_text, question_ans)
     question_bank.append(question)
 
